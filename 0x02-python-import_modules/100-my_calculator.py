e0e1847507113c177f66e242f4c782864d2375af
Espoir <muhiespoir2020@gmail.com>
2023-12-19 13:57:51 -0800

    correction of errors

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -19,7 +19,6 @@ elif argv[2] == '/':
 else:
     print("Unknown operator. Available operators: +, -, *, and /")
     exit(1)
-
-result = func(int(argv[1]), int(argv[3]))
-print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
+    result = func(int(argv[1]), int(argv[3]))
+    print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
     argv[2], int(argv[3]), result))
