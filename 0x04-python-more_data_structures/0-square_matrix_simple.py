3bc7ac84885d721ee380de42a5b35ffb1a6c649e
Espoir <muhiespoir2020@gmail.com>
2024-02-09 09:51:12 -0800

    correction on error on one file

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def squared(element):
-    return (element**2)
-
-
 def square_matrix_simple(matrix=[]):
-    new = matrix[:]
-    for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                new[i] = list(map(squared, matrix[i]))
-    return new
+    response = []
+    for row in matrix:
+        sub_matrix = map(lambda num: num**2, row)
+        response.append(list(sub_matrix))
+    return response
