0287fc9bfd77482f9e953e0c3bdd827d047c9320
Espoir <muhiespoir2020@gmail.com>
2024-03-21 07:39:05 -0700

    commit of correction of errors

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -16,3 +16,11 @@ def max_integer(list=[]):
             result = list[i]
         i += 1
     return result
+
+guillaume@ubuntu:~/0x07$ 
+guillaume@ubuntu:~/0x07$ cat 6-main.py
+#!/usr/bin/python3
+max_integer = __import__('6-max_integer').max_integer
+
+print(max_integer([1, 2, 3, 4]))
+print(max_integer([1, 3, 4, 2]))
